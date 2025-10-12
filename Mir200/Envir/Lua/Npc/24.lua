f8879879083f0d4a94090fb98a84ce49ad66e319
lll <2418759512@qq.com>
2025-10-13 00:26:47 +0800

    10.13

---
+++ Mir200/Envir/Lua/Npc/24.lua
@@ -0,0 +1,16 @@
+npc = {}
+--ÃÏ È
+
+local _config = teshudata["npc_24"]
+
+function npc.main(play,npcid)
+    sendluamsg(play,100,npcid,0,0,"")
+end
+
+function npc.link(play,npcid,ew,aid)
+    if ew == 1 then
+
+    end
+end
+
+return npc
