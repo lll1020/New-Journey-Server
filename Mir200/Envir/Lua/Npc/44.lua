338239ab1d99fdc718ea0d49a9932544d6c1c5df
lll <2418759512@qq.com>
2025-11-20 21:21:32 +0800

    装备 和部分配置

--- Mir200/Envir/Lua/Npc/44.lua
+++ Mir200/Envir/Lua/Npc/44.lua
@@ -8,7 +8,7 @@ local _config = Guard.getConfig("npc_44")
 function npc.main(play,npcid)
     local jq_data = Player.getJsonTableByVar(play, VarCfg.T_dljq)
     if jq_data["npc55"] and jq_data["npc55"] >= 2 then
-        sendluamsg(play,100,npcid,0,0,tbl2json(data))
+        sendluamsg(play,100,npcid,0,0,tbl2json(Player.getJsonTableByVar(play, VarCfg["T_仙府"])))
     else -- 未完成该前置任务
         Player.sendmsgEx(play,  "你还未完成开辟仙府任务，无法进行该操作#57")
         return
@@ -31,9 +31,10 @@ function npc.link(play, npcid, p2, p3, msgData)
         return
     end
 
-    if p2 == 1 then
+    local T_data = Player.getJsonTableByVar(play,VarCfg["T_仙府"])
+    if p2 == 1 then ---灵草种植
        
-    elseif p2 == 2 then
+    elseif p2 == 2 then  ---府邸装扮
         
     end
 end
