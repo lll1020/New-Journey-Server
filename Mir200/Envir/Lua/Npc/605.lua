4ab4f51ca0e2f931183e90956401e30702c68b94
lll <2418759512@qq.com>
2026-03-06 17:32:34 +0800

    x小调整

--- Mir200/Envir/Lua/Npc/605.lua
+++ Mir200/Envir/Lua/Npc/605.lua
@@ -59,7 +59,7 @@ function npc.link(play,npcid,ew,aid)
                 Player.setJsonVarByTable(play, VarCfg.T_dljq, jq_data)
                 Player.sendmsgEx(play, "【"..(_config.name or "任务").."】完成")
                 sendluamsg(play,101,1005,0,0,"rwwc")
-                Player.rwjl(play, _config.rwjl or {{"元宝",1},{"金币",1}}, (_config.name or "剧情任务").."奖励", 1)
+                Player.rwjl(play, _config.rwjl or {{"绑定元宝",1},{"绑定金币",1}}, (_config.name or "剧情任务").."奖励", 1)
                 sendluamsg(play,100,npcid,1,2,"")
             else
                 Player.sendmsgEx(play, "你还没有完成【"..(_config.name or "该任务").."】#57")
